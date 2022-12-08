44998cfbbe18b7db04e333bf0520f1e0ea0d58c0
Louis Zhu <xiaofan.zhu@aicure.com>
2022-12-08 10:23:53 +0800

    initial commit

---
+++ src/test/java/com/lepinardsoft/league/LeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lepinardsoft.league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
